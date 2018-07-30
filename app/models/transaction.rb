2aa6817239fa4b6cf88a1cb90d2b5b96cc13866d
vgty <victor.goutay@gmail.com>
2018-07-30 15:11:30 +0200

    #Exercice 2 done

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,6 +1,6 @@
 class Transaction < ApplicationRecord
   belongs_to :account
-  before_create :credit_or_debit
+  before_save :credit_or_debit
   
   def credit_or_debit
     self.amount = 0 - self.amount if self.out
